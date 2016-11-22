a151cb6279e68eb013b687dfc9f2ca889e81c8ca
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-11-22 14:22:54 +0100

    add major network code for udacity api (auth) calls

---
+++ OnTheMap/LoginViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewControllerExtension.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 22.11.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
