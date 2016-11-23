aca53088c308cced156f5d8c2a368742330b9759
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-11-23 11:42:10 +0100

    add new udc session model

---
+++ OnTheMap/UDCSession.swift
@@ -0,0 +1,19 @@
+//
+//  UDCSession.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 23.11.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+
+struct UDCSession {
+
+    let accountKey: String!
+    let accountRegistered: Bool!
+    let sessionId: String!
+    let sessionExpirationDate: Date!
+    
+    let created: Date!
+}
