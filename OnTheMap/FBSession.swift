5e8cdcad0188dbe5d333174fa4967479d21ac931
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-11-28 22:30:40 +0100

    add facebook session object storage (2/n)

--- OnTheMap/FBSession.swift
+++ OnTheMap/FBSession.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct FBSession {
+    
+    let tokenString: String!
+    
+    let email: String!
+    let userID: String!
+    let userImgUrl: String!
+    
+    let appID: String!
+    let permissions: Set<AnyHashable>?
+    
+    let expirationDate: Date!
+    let refreshDate: Date!
+    
+    let created: Date!
+}
