ab08d18b80e5cc7e19fe4093f5915539274dcb6d
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-12-26 13:25:48 +0100

    add new locationViewController

---
+++ OnTheMap/LocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationViewController.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 26.12.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UITabBarController {
+    
+}
