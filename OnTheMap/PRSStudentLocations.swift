73c8a5e550b8590870b4e31c43b350b1260662ca
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-08 15:18:07 +0100

    add parse client studen location persistent collection holder

---
+++ OnTheMap/PRSStudentLocations.swift
@@ -0,0 +1,16 @@
+//
+//  PRSStudentLocations.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 08.03.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+
+class PRSStudentLocations {
+    
+    static let sharedInstance = PRSStudentLocations()
+    
+    var locations = [PRSStudentData]()
+}
