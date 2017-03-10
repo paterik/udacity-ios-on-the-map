ea073ae5f0200afc835447ac04c14c275972a53f
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-11 00:59:39 +0100

    add new partial based annotation map view (xib class)

---
+++ OnTheMap/StudentMapAnnotation.swift
@@ -0,0 +1,21 @@
+//
+//  StudentMapAnnotation.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 10.03.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import UIKit
+
+class StudentMapAnnotation: UIView {
+    
+    @IBOutlet weak var studentImage: UIImageView!
+    @IBOutlet weak var studentName: UILabel!
+    @IBOutlet weak var studentLinkGithub: UIButton!
+    @IBOutlet weak var studentLinkTwitter: UIButton!
+    @IBOutlet weak var studentLinkXing: UIButton!
+    @IBOutlet weak var studentLinkLinkedIn: UIButton!
+    @IBOutlet weak var studentDistance: UILabel!
+
+}
