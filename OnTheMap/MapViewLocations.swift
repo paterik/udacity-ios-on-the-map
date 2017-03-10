24cccfcb205a69e9b42ad403acacaa7e356626f6
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-10 18:44:09 +0100

    add map location persitence layer

---
+++ OnTheMap/MapViewLocations.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewLocations.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 10.03.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+
+class MapViewLocations {
+    
+    static let sharedInstance = MapViewLocations()
+    
+    var currentLocations = [MapViewLocation]()
+}
