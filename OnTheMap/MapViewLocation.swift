341846bcd8ca7eb76b348ee6057ca04a718f6a7d
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-10 18:43:36 +0100

    add map location entity

---
+++ OnTheMap/MapViewLocation.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewLocation.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 10.03.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import UIKit
+
+struct MapViewLocation {
+    
+    let latitude: Double!
+    let longitude: Double!
+    let evaluated: Date!
+    
+    init (_ dictionary: NSDictionary) {
+        
+        latitude  = (dictionary["latitude"] as? Double!)  ?? nil
+        longitude = (dictionary["longitude"] as? Double!) ?? nil
+        
+        evaluated = Date()
+    }
+}
