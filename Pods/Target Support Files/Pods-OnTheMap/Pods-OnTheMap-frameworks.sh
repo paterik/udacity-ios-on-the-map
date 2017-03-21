ef7d078aa2eb70e952803caa656c410886d331b7
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-21 19:14:12 +0100

    remove class definition for BGTableViewRowActionWithImage

--- Pods/Target Support Files/Pods-OnTheMap/Pods-OnTheMap-frameworks.sh
+++ Pods/Target Support Files/Pods-OnTheMap/Pods-OnTheMap-frameworks.sh
@@ -91,7 +91,6 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AWSCore/AWSCore.framework"
   install_framework "$BUILT_PRODUCTS_DIR/AWSS3/AWSS3.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/BGTableViewRowActionWithImage/BGTableViewRowActionWithImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FBSDKCoreKit/FBSDKCoreKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FBSDKLoginKit/FBSDKLoginKit.framework"
@@ -100,7 +99,6 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AWSCore/AWSCore.framework"
   install_framework "$BUILT_PRODUCTS_DIR/AWSS3/AWSS3.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/BGTableViewRowActionWithImage/BGTableViewRowActionWithImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FBSDKCoreKit/FBSDKCoreKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FBSDKLoginKit/FBSDKLoginKit.framework"
