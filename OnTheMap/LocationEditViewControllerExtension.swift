bd2648a5fdcea87d7b23ac95266367674b1a8321
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-23 19:55:17 +0100

    add location marker (for plot and final position in manual mode)

--- OnTheMap/LocationEditViewControllerExtension.swift
+++ OnTheMap/LocationEditViewControllerExtension.swift
@@ -10,7 +10,7 @@
 import UIKit
 import MapKit
 
-extension LocationEditViewController {
+extension LocationEditViewController: MKMapViewDelegate {
 
     /*
      * get map location by parametric location string
@@ -74,7 +74,56 @@ extension LocationEditViewController {
         btnSubmit.setTitle("Confirm Location", for: .normal)
         btnState = 2
         
+        // add map annotation pin
+        DispatchQueue.main.async { self.mapView.addAnnotation(PRSStudentMapAnnotation(coordinate)) }
+        
         activitySpinner.stopAnimating()
         view.willRemoveSubview(activitySpinner)
+    }
+    
+    /*
+     * update temporary user location icon by a "solid" one
+     */
+    func updateMapLocationAnnotationAsConfirmed() {
+        
+        for annotation in mapView.annotations {
+            let annotationView = mapView.view(for: annotation)
+            
+            if annotationView?.annotation is PRSStudentMapAnnotation {
+                annotationView?.image = UIImage(named: "icnUserDefault_v1")
+            }
+        }
+    }
+    
+    //
+    // MARK: Delegates
+    //
+    
+    func mapView(
+        _ mapView: MKMapView,
+        viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        // ignore all given location annotation except the student ones
+        if !(annotation is PRSStudentMapAnnotation) {
+            return nil
+        }
+        
+        let identifier = "locPin_0"
+        var annotationView = self.mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            
+            annotationView = StudentMapAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = false
+            
+        } else {
+            
+            annotationView?.annotation = annotation
+            
+        }
+        
+        annotationView?.image = UIImage(named: "icnUserTemp_v1")
+        
+        return annotationView
     }
 }
