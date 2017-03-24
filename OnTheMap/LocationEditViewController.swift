ce32bb5772f2a1732996c29721a0106feb0f1dbb
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-24 20:41:29 +0100

    add new locationEditView controller base, massive refactoring in core code

--- OnTheMap/LocationEditViewController.swift
+++ OnTheMap/LocationEditViewController.swift
@@ -2,109 +2,66 @@
 //  LocationEditViewController.swift
 //  OnTheMap
 //
-//  Created by Patrick Paechnatz on 13.03.17.
+//  Created by Patrick Paechnatz on 24.03.17.
 //  Copyright © 2017 Patrick Paechnatz. All rights reserved.
 //
 
 import UIKit
-import MapKit
 
-class LocationEditViewController: UIViewController, EditViewProtocol {
-
-    //
-    // MARK: Constants
-    //
-    let debugMode: Bool = false
-    let clientParse = PRSClient.sharedInstance
-    let clientUdacity = UDCClient.sharedInstance
-    let locationMapZoom : CLLocationDegrees = 0.03 // zoom factor (0.03 seems best for max zoom)
+class LocationEditViewController: UIViewController {
     
     //
     // MARK: Variables
     //
-    var useCurrentDeviceLocation: Bool = false
-    var btnState: Int = 1 // initial state of confirm button 1: Plot Location, 2: Submit Location
-    var activitySpinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+    
+    var locationPageViewController: LocationPageViewController? {
+        didSet { locationPageViewController?.locationDelegate = self }
+    }
     
     //
     // MARK: IBOutlet Variables
     //
-    @IBOutlet weak var btnCloseView: UIBarButtonItem!
-    @IBOutlet weak var btnAcceptLocation: UIBarButtonItem!
-    @IBOutlet weak var inpLocationMapString: UITextField!
-    @IBOutlet weak var btnSubmit: UIButton!
-    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var containerView: UIView!
+    
+    //
+    // MARK: Overrides
+    //
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        activitySpinner.center = self.view.center
-        
-        mapView.delegate = self
-        mapView.showsUserLocation = false
+        pageControl.addTarget(
+            self,
+            action: #selector(LocationEditViewController.btnPageControlAction),
+            for: .valueChanged
+        )
     }
     
-    //
-    // MARK: IBOutlet Actions
-    //
-    @IBAction func btnHandleLocationSubmitAction(_ sender: Any) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        btnSubmit.isEnabled = true
-        inpLocationMapString.isEnabled = false
-        btnAcceptLocation.isEnabled = false
-        
-        // evaluate state and define title and action
-        switch btnState {
-        
-            case 1:
-                
-                // perform step 1 action
-                getMapPositionByAddressString(inpLocationMapString.text!)
-
-                break
-            
-            case 2:
-                
-                // perform step 2 action
-                updateMapLocationAnnotationAsConfirmed()
-
-                break
-            
-            case 3:
-                
-                // perform step 3 action
-                btnAcceptLocationAction(sender)
-                
-                break
-            
-            default:
-                
-                // leave view on any state below 1 or above 3
-                btnCloseViewAction(sender)
-                
-                break
+        if let tutorialPageViewController = segue.destination as? LocationPageViewController {
+            self.locationPageViewController = tutorialPageViewController
         }
     }
     
-    @IBAction func btnCloseViewAction(_ sender: Any) {
-        self.dismiss(animated: true)
+    //
+    // MARK: IBAction Methods
+    //
+    
+    @IBAction func btnNextPageAction(_ sender: Any) {
+        locationPageViewController?.scrollToNextViewController()
     }
     
-    @IBAction func btnAcceptLocationAction(_ sender: Any) {
+    //
+    // MARK: Methods
+    //
     
-        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        var vc: ProfileEditViewController!
-        
-        vc = storyBoard.instantiateViewController(withIdentifier: "profileEditView") as! ProfileEditViewController
-        vc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
-        vc.modalPresentationStyle = UIModalPresentationStyle.fullScreen
-        
-        vc.useCurrentDeviceLocation = false
-        vc.mapView = self.mapView
-        
-        self.present(vc, animated: true, completion: nil)
+    func btnPageControlAction() {
+        locationPageViewController?.scrollToViewController(index: pageControl.currentPage)
     }
-    
-    @IBAction func btnSetLocationToCurrentDeviceLocationAction(_ sender: Any) {}
 }
+
+
--- OnTheMap/LocationEditViewControllerExtension.swift
+++ OnTheMap/LocationEditViewControllerExtension.swift
@@ -2,168 +2,25 @@
 //  LocationEditViewControllerExtension.swift
 //  OnTheMap
 //
-//  Created by Patrick Paechnatz on 22.03.17.
+//  Created by Patrick Paechnatz on 24.03.17.
 //  Copyright © 2017 Patrick Paechnatz. All rights reserved.
 //
 
+import Foundation
 
-import UIKit
-import MapKit
-
-extension LocationEditViewController: MKMapViewDelegate {
-
-    /*
-     * get map location by parametric location string
-     */
-    func getMapPositionByAddressString (
-        _ address: String) {
+extension LocationEditViewController: LocationPageViewControllerDelegate {
+    
+    func locationPageViewController(
+       _ locationPageViewController: LocationPageViewController,
+         didUpdatePageCount count: Int) {
         
-        activitySpinner.startAnimating()
-        view.addSubview(activitySpinner)
-        
-        let _geocoder = CLGeocoder()
-        
-        if debugMode == true {
-            print ("-------------------------------------------------------------------")
-            print ("try to find: \(address)")
-            print ("-------------------------------------------------------------------")
-        }
-        
-        _geocoder.geocodeAddressString(
-            address,
-            completionHandler: {(placemarks, error) -> Void in
-                
-                if error != nil {
-                    
-                    self.activitySpinner.stopAnimating()
-                    self.view.willRemoveSubview(self.activitySpinner)
-                    
-                    let alertController = UIAlertController(
-                        title: "Alert",
-                        message: "Up's, I'm unable to find your defined location '\(address)' ... try another one!",
-                        preferredStyle: UIAlertControllerStyle.alert
-                    )
-                    
-                    let Action = UIAlertAction(title: "Try again", style: .default) { (action:UIAlertAction!) in return }
-                        alertController.addAction(Action)
-                    
-                    OperationQueue.main.addOperation { self.present(alertController, animated: true, completion:nil)}
-                }
-                
-                if let placemark = placemarks?.first {
-                    
-                    let coordinate:CLLocationCoordinate2D = placemark.location!.coordinate
-                    self.generateMapLocationByCoordinates(coordinate)
-                }
-            }
-        )
+        pageControl.numberOfPages = count
     }
     
-    /*
-     * set mapView to parametric location coordinates
-     */
-    func generateMapLocationByCoordinates(
-        _ coordinate: CLLocationCoordinate2D) {
+    func locationPageViewController(
+       _ locationPageViewController: LocationPageViewController,
+         didUpdatePageIndex index: Int) {
         
-        let region = MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude),
-            span: MKCoordinateSpan(latitudeDelta: locationMapZoom, longitudeDelta: locationMapZoom)
-        )
-        
-        mapView.setRegion(region, animated: true)
-        DispatchQueue.main.async { self.mapView.addAnnotation(PRSStudentMapAnnotation(coordinate)) }
-        
-        activitySpinner.stopAnimating()
-        view.willRemoveSubview(activitySpinner)
-        
-        // prepare ui for step 2
-        self.prepareStep(2)
-    }
-    
-    /*
-     * update temporary user location icon by a "solid" one
-     */
-    func updateMapLocationAnnotationAsConfirmed() {
-        
-        for annotation in mapView.annotations {
-            let annotationView = mapView.view(for: annotation)
-            
-            if annotationView?.annotation is PRSStudentMapAnnotation {
-                annotationView?.image = UIImage(named: "icnUserDefault_v1")
-            }
-        }
-        
-        // prepare ui for step 2
-        prepareStep(3)
-    }
-    
-    /*
-     * simple ui element modifier for our corresponding steps (1-3)
-     */
-    func prepareStep(_ step: Int8) {
-        
-        switch step {
-            
-            case 1:
-            
-                inpLocationMapString.resignFirstResponder()
-                btnSubmit.setTitle("Plot Location", for: .normal)
-                btnState = 1
-            
-                break
-            
-            case 2:
-            
-            
-                btnAcceptLocation.isEnabled = true
-                inpLocationMapString.isEnabled = false
-                btnSubmit.setTitle("Confirm Location", for: .normal)
-                btnState = 2
-            
-                break
-            
-            case 3:
-            
-                btnAcceptLocation.isEnabled = true
-                inpLocationMapString.isEnabled = false
-                btnSubmit.setTitle("Set Your Profile", for: .normal)
-                btnState = 3
-            
-                break
-            
-            default: break
-        }
-    }
-    
-    //
-    // MARK: Delegates
-    //
-    
-    func mapView(
-        _ mapView: MKMapView,
-        viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        // ignore all given location annotation except the student ones
-        if !(annotation is PRSStudentMapAnnotation) {
-            return nil
-        }
-        
-        let identifier = "locPin_0"
-        var annotationView = self.mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-        
-        if annotationView == nil {
-            
-            annotationView = StudentMapAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            annotationView?.canShowCallout = false
-            
-        } else {
-            
-            annotationView?.annotation = annotation
-            
-        }
-        
-        annotationView?.image = UIImage(named: "icnUserTemp_v1")
-        
-        return annotationView
+        pageControl.currentPage = index
     }
 }
