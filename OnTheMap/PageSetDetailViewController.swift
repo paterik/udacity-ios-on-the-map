ef3c7bbb5f305d5003299359803d49b84399e507
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-24 20:43:39 +0100

    add (new) pageSetDetailView controller and extension

---
+++ OnTheMap/PageSetDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PageSetDetailViewController.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 24.03.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import UIKit
+
+class PageSetDetailViewController: UIViewController {
+
+    @IBOutlet weak var btnCloseView: UIBarButtonItem!
+    
+    @IBAction func btnCloseViewAction(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+}
---
+++ OnTheMap/PageSetDetailViewControllerExtension.swift
@@ -0,0 +1,17 @@
+//
+//  PageSetDetailViewControllerExtension.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 24.03.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+extension PageSetDetailViewController: UIPageViewControllerDelegate {
+
+    //
+    // MARK: Delegates
+    //
+}
