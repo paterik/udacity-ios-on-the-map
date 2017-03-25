a955d5aedadca2e9c3d952ca5bf5827cf316b57c
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-25 11:42:36 +0100

    add new delegate protocol to handle commands from embedded pageView controllers

---
+++ OnTheMap/PageSetViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PageSetViewController.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 25.03.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import UIKit
+
+
+class PageSetViewController: UIViewController {
+    
+    var delegate: PageSetViewControllerProtocol?
+    
+}
