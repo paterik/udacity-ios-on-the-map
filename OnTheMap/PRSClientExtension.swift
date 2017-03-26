054a514fce97dd3b8d26fd28b5ece5312c3a346f
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-26 12:18:19 +0200

    refatoring of parse client extension

--- OnTheMap/PRSClientExtension.swift
+++ OnTheMap/PRSClientExtension.swift
@@ -11,10 +11,10 @@ import Foundation
 extension PRSClient {
 
     /*
-     * prepare student data object for upcoming post request remove post-obsolete key 'objectId'
+     * prepare student data object for upcoming post request remove redundant key 'objectId'
      */
     func prepareStudentMetaForPostRequest(
-        _ studentData: PRSStudentData?) -> [String : AnyObject]? {
+       _ studentData: PRSStudentData?) -> [String : AnyObject]? {
         
         var studentDataArray = studentData!.asArray
         if let index = studentDataArray.index(forKey: "objectId") {
@@ -28,19 +28,19 @@ extension PRSClient {
      * return json string from array
      */
     func getJSONFromStringArray(
-        _ arrayData: [String:String]) -> String {
+       _ arrayData: [String:String]) -> String {
         
         var JSONString: String = "{}"
         
         do {
             
             let jsonData = try JSONSerialization.data(withJSONObject: arrayData, options: JSONSerialization.WritingOptions.prettyPrinted)
-            if let _jsonString = String(data: jsonData, encoding: String.Encoding.utf8) {
-                JSONString = _jsonString
-            }
+            if let _jsonString = String(data: jsonData, encoding: String.Encoding.utf8) { JSONString = _jsonString }
             
         } catch {
+            
             if debugMode { print ("An Error occured in ParseClient::getJSONFromStringArray -> \(error)") }
+        
         }
         
         return JSONString
@@ -50,16 +50,14 @@ extension PRSClient {
      * return json string from student meta dictionary
      */
     func getJSONFromStudentMetaDictionary(
-        _ studentData: PRSStudentData!) -> String {
+       _ studentData: PRSStudentData!) -> String {
         
         var JSONString: String = "{}"
         
         do {
             
             let jsonData = try JSONSerialization.data(withJSONObject: studentData, options: JSONSerialization.WritingOptions.prettyPrinted)
-            if let _jsonString = String(data: jsonData, encoding: String.Encoding.utf8) {
-                JSONString = _jsonString
-            }
+            if let _jsonString = String(data: jsonData, encoding: String.Encoding.utf8) { JSONString = _jsonString }
             
         } catch {
             if debugMode { print ("An Error occured in ParseClient::getJSONFromStringArray -> \(error)") }
@@ -72,20 +70,21 @@ extension PRSClient {
      * validate incoming student meta lines check for valid geo localization properties, return false if
      * coordinates seems invalid (using regex validation process)
      */
-    func validateStudentMeta(_ meta:PRSStudentData) -> Bool {
+    func validateStudentMeta(
+       _ meta:PRSStudentData) -> Bool {
+        
+        var isValid: Bool = false
         
         guard let _latitudeRaw = meta.latitude,
               let _longitudeRaw = meta.longitude else {
             
-            return false
+            return isValid
         }
         
         let _latitude: String = String(format:"%f", _latitudeRaw)
         let _longitude: String = String(format:"%f", _longitudeRaw)
         let _latitudeRegex = "^(\\+|-)?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$"
         let _longitudeRegex = "^(\\+|-)?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$"
-        
-        var isValid: Bool = false
         
         if (_longitude.range(of: _longitudeRegex, options: .regularExpression) != nil &&
             _latitude.range(of: _latitudeRegex, options: .regularExpression) != nil) {
