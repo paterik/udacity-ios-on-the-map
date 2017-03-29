64408f5fbe9f03942f515f85bec672fe54ca3513
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-29 21:51:50 +0200

    update pod definition(s) add BGTableViewRow lib

--- Pods/Target Support Files/Pods-OnTheMap/Pods-OnTheMap-frameworks.sh
+++ Pods/Target Support Files/Pods-OnTheMap/Pods-OnTheMap-frameworks.sh
@@ -91,6 +91,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AWSCore/AWSCore.framework"
   install_framework "$BUILT_PRODUCTS_DIR/AWSS3/AWSS3.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/BGTableViewRowActionWithImage/BGTableViewRowActionWithImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FBSDKCoreKit/FBSDKCoreKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FBSDKLoginKit/FBSDKLoginKit.framework"
@@ -99,6 +100,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AWSCore/AWSCore.framework"
   install_framework "$BUILT_PRODUCTS_DIR/AWSS3/AWSS3.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/BGTableViewRowActionWithImage/BGTableViewRowActionWithImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FBSDKCoreKit/FBSDKCoreKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FBSDKLoginKit/FBSDKLoginKit.framework"
