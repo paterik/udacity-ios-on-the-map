bc928edab15b6f7ed821b51f2ab6ec9953f24e04
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-03-29 21:50:50 +0200

    add new studentTableCell xib definition(s)

---
+++ OnTheMap/StudentTableCell.swift
@@ -0,0 +1,9 @@
+//
+//  StudentTableCell.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 29.03.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
