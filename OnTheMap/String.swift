c01aad9295c6ee7eb09ea8b7f6db9de650d2ed10
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-04-01 13:27:36 +0200

    move validateMediaUrl as string extension

--- OnTheMap/String.swift
+++ OnTheMap/String.swift
@@ -11,10 +11,30 @@ import Foundation
 extension String {
     
     func chopPrefix(_ count: Int = 1) -> String {
+        
         return self.substring(from: self.characters.index(self.startIndex, offsetBy: count))
     }
     
     func chopSuffix(_ count: Int = 1) -> String {
+        
         return self.substring(to: self.characters.index(self.startIndex, offsetBy: count))
+    }
+    
+    func validateMediaURL() -> Bool {
+        
+        let types: NSTextCheckingResult.CheckingType = [.link]
+        let detector = try? NSDataDetector(types: types.rawValue)
+        
+        guard (detector != nil && self.characters.count > 0) else { return false }
+        
+        if detector!.numberOfMatches(
+            in: self,
+            options: NSRegularExpression.MatchingOptions(rawValue: 0),
+            range: NSMakeRange(0, self.characters.count)) > 0 {
+            
+            return true
+        }
+        
+        return false
     }
 }
