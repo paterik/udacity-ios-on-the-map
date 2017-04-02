10a791dc071ad4c9d9b5d194ef57a38c8859ecfb
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-04-02 12:55:04 +0200

    add formField validator closure, add facebook auth udacity session fetch, minor refactoring

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -16,6 +16,7 @@ class LoginViewController: UIViewController {
     //
     // MARKS: Outlets
     //
+    
     @IBOutlet weak var inpUdacityUser: UITextField!
     @IBOutlet weak var inpUdacityPassword: UITextField!
     @IBOutlet weak var btnLoginUdactiy: UIButton!
@@ -26,6 +27,7 @@ class LoginViewController: UIViewController {
     //
     // MARKS: Constants
     //
+    
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     let clientUdacity = UDCClient.sharedInstance
     let clientFacebook = FBClient.sharedInstance
@@ -33,6 +35,7 @@ class LoginViewController: UIViewController {
     //
     // MARKS: Variables
     //
+    
     var activitySpinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
     
     override func viewDidLoad() {
@@ -57,87 +60,130 @@ class LoginViewController: UIViewController {
             "https://auth.udacity.com/sign-in?next=https%3A%2F%2Fclassroom.udacity.com%2Fauthenticated"
         )
     }
+    
+    /*
+     * check input fields for udacity login process and return error on any kind of problem
+     */
+    func validateUsernameAndPassword(completionHandlerFieldValidator: @escaping (
+       _ success: Bool,
+       _ message: String?,
+       _ username: String?,
+       _ password: String?)
+        
+        -> Void) {
+    
+        guard let _username = inpUdacityUser.text else {
+            completionHandlerFieldValidator(false, "Up's, missing username in your authentication request!", nil, nil)
+            
+            return
+        }
+        
+        guard let _password = inpUdacityPassword.text else {
+            completionHandlerFieldValidator(false, "Up's, missing password in your authentication request!", nil, nil)
+            
+            return
+        }
+        
+        if _password.isEmpty || _username.isEmpty {
+            completionHandlerFieldValidator(false, "Up's, missing field data for your authentication request!", nil, nil)
+            
+            return
+        }
+        
+        completionHandlerFieldValidator(true, nil, _username, _password)
+    }
 
     @IBAction func loginUdacityAction(_ sender: AnyObject) {
         
-        // show activity spinner
-        activitySpinner.startAnimating()
-        view.addSubview(activitySpinner)
-        
-        // deactivate ui during http rest call
         activateUI(false)
         
-        clientUdacity.getUserSessionDeveloperToken() { (udcSession, error) in
-                
-            if error == nil {
+        validateUsernameAndPassword() {
+        
+            (success, message, username, password) in
+            
+            if success == true {
+        
+                self.clientUdacity.getUserSessionToken(username, password, nil) {
                     
-                self.appDelegate.isAuthByUdacity = true
-                self.appDelegate.setUdacitySession(udcSession!)
-                self.clientUdacity.clientSession = self.appDelegate.getUdacitySession()
-                
-                self.loadLocationViewController()
+                    (udcSession, error) in
                     
-                // cleanOut username and password after login done successfully
-                OperationQueue.main.addOperation {
-                    self.inpUdacityUser.text = ""
-                    self.inpUdacityPassword.text = ""
+                    if error == nil {
+                        
+                        // cleanOut username and password after login done successfully
+                        OperationQueue.main.addOperation {
+                            
+                            self.inpUdacityUser.text = ""
+                            self.inpUdacityPassword.text = ""
+                            
+                            self.appDelegate.isAuthByUdacity = true
+                            self.appDelegate.isAuthByFacebook = false
+                            self.appDelegate.setUdacitySession(udcSession!)
+                            self.clientUdacity.clientSession = self.appDelegate.getUdacitySession()
+                            
+                            self.loadLocationViewController()
+                        }
+                        
+                    } else {
+                        
+                        self.showErrorMessage(error!)
+                    }
                 }
-                    
-            } else {
-
-                self.showErrorMessage(error!)
-                self.activateUI(true)
-            }
                 
-            // (re)activate ui after http rest call result handling
-            self.activateUI(true)
+            } else {
+            
+                self.showErrorMessage(message!)
+            }
         }
     }
     
     @IBAction func loginFacebookAction(_ sender: AnyObject) {
         
-        // show activity spinner
-        activitySpinner.startAnimating()
-        view.addSubview(activitySpinner)
-        
-        // deactivate ui during http rest call
         activateUI(false)
 
         clientFacebook.getUserSessionToken (viewController: self) { (success: Bool?, fbSession: FBSession?, message: String?) in
-
+            
             if success! == true {
                 
-                // persist udacity session model and provide it inside appDelegate globaly
                 self.appDelegate.isAuthByFacebook = true
+                self.appDelegate.isAuthByUdacity = false
                 self.appDelegate.setFacebookSession(fbSession!)
                 self.clientFacebook.clientSession = self.appDelegate.getFacebookSession()
                 
-                print (self.clientFacebook.clientSession!)
+                let fbSessionToken = self.clientFacebook.clientSession?.tokenString!
                 
-                self.loadLocationViewController()
-
+                self.clientUdacity.getUserSessionToken(nil, nil, fbSessionToken) {
+                    
+                    (udcSession, error) in
+                
+                    if error == nil {
+                    
+                        OperationQueue.main.addOperation {
+                            self.appDelegate.setUdacitySession(udcSession!)
+                            self.clientUdacity.clientSession = self.appDelegate.getUdacitySession()
+                            self.loadLocationViewController()
+                        }
+                        
+                    } else {
+                        
+                        self.showErrorMessage(error!)
+                    }
+                }
+                
             } else {
                 
-                self.showErrorMessage(message!)                
+                self.showErrorMessage(message!)
             }
-
-            // (re)activate ui after http rest call result handling
-            self.activateUI(true)
         }
     }
     
     private func loadLocationViewController() {
     
+        activateUI(true)
+        
         let locationViewController = self.storyboard!.instantiateViewController(
             withIdentifier: "LocationTabViewController") as! LocationTabViewController
         
         locationViewController.modalTransitionStyle = UIModalTransitionStyle.coverVertical
-    
-        // deactivate and remove activity spinner
-        activitySpinner.stopAnimating()
-        view.willRemoveSubview(self.activitySpinner)
-        
-        self.activateUI(true)
         
         self.present(locationViewController, animated: true, completion: nil)
     }
