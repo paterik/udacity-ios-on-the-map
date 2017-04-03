c00d71cae27bb98df15a3132d91f2afa9f199fc7
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-04-03 15:16:06 +0200

    switch deviceLocation fetch mode (using simple/quick mode now)

--- OnTheMap/MapViewController.swift
+++ OnTheMap/MapViewController.swift
@@ -35,7 +35,7 @@ class MapViewController: BaseController, ControllerCommandProtocol {
     let locationMapZoom : CLLocationDegrees = 0.03 // zoom factor (0.03 seems best for max zoom)
     let locationDistanceDivider : Double = 1000.0  // rate for metric conversion (m -> km)
     let locationDistanceHook : Double = 50         // minimum distance (measured in meters) before an update
-    let locationFetchMode : Int8 = 1               // 1: saveMode, 2: quickMode
+    let locationFetchMode : Int8 = 2               // 1: saveMode, 2: quickMode
     let locationCoordRound : Int = 6               // round factor for coordinate comparison
     let locationNoData : String = "no meta data"   // default for missing student meta data
     
