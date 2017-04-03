8b1dcb6d0576796cff34e4dad1618fb133128540
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-04-03 11:08:43 +0200

    addnew table cell statistic for listView

---
+++ OnTheMap/StudentTableCellStatistic.swift
@@ -0,0 +1,16 @@
+//
+//  StudentTableCellStatistic.swift
+//  OnTheMap
+//
+//  Created by Patrick Paechnatz on 03.04.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import UIKit
+
+class StudentTableCellStatistic: UITableViewCell {
+ 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
