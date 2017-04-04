bbbe0195bd724a1b031ee0c13bd288cbd61fa7e4
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-04-04 18:14:10 +0200

    add docLine in locationTabViewController

--- OnTheMap/LocationTabViewController.swift
+++ OnTheMap/LocationTabViewController.swift
@@ -2,6 +2,8 @@
 //  LocationTabViewController.swift
 //  OnTheMap
 //
+//  -/- not used yet
+//
 //  Created by Patrick Paechnatz on 26.12.16.
 //  Copyright © 2016 Patrick Paechnatz. All rights reserved.
 //
